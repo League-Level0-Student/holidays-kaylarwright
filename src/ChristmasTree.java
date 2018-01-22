3d0d8066fea6ad88069e95ab5521b025b3f031e3
Student <Student@10.48.130.46>
2018-01-22 14:23:30 -0800

    Holidays

--- src/ChristmasTree.java
+++ src/ChristmasTree.java
@@ -5,12 +5,14 @@
  *    
  ******************************************************************************/
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 
 public class ChristmasTree {
 
-	Robot rob  = new Robot();
+	Robot rob  = new Robot("mini");
 	
     public static void main(String[] args) {
         ChristmasTree ohChristmasTree = new ChristmasTree();
@@ -25,56 +27,65 @@ public class ChristmasTree {
 
 
     void drawTreeBody() {
-        // 8. Change the color of the line the robot draws to forest green    
-
-
+        // 8. Change the color of the line the robot draws to forest green   
+    	rob.setPenColor(Color.GREEN);
         // 1. Make a variable for turnAmount and set it to 175
-
+int turnAmount = 175;
 
         // 2. Start the Robot facing to the right
+rob.setAngle(90);
 
 
         // 5. Repeat steps 3 through 11, 11 times
 
-
+for (int i = 0; i < 11; i++) {
             // 3. Move the robot the width of the tree
+rob.move((int) treeWidth);
     
             // 4. Turn the robot the current turnAmount to the right
-    
+rob.turn(turnAmount);
+
             // 6. Set the treeWidth to the current treeWidth times the scale
-    
+    treeWidth = treeWidth * scale;
             // 7. Move the robot the width of a tree again
-    
+rob.move((int) treeWidth);
             // 9. Turn the robot the current turn amount to the LEFT
-    
+rob.turn(-turnAmount);
             // 10. Set the treeWidth to the current treeWidth times the scale again
-    
+    treeWidth = treeWidth * scale;
             // 11. Decrease turnAmount by 1
-
-
+turnAmount--;
+}
 }
     
     void drawTreeTrunk() {
         // 1. Move the robot half the width of the tree
-
+rob.move((int) (treeWidth/2));
 
         // 2. Change the robot so that it is pointing straight down
-
+rob.turn(90);
 
         // 4. Set the pen width to the tree width divided by 10
-
+rob.setPenWidth((int) (treeWidth/10));
 
         // 5. Change the color of the line the robot draws to brown
-
+rob.setPenColor(200, 150, 100);
 
         // 3. Move the robot a quarter the tree width
-
+rob.move((int) (treeWidth/4));
 
     }
     
     void drawStar() {
         // * Optional: Draw a red star on top of the tree. Hint: 144 degrees makes a star.
-    
+    	rob.setSpeed(100);
+    	rob.penDown();
+    	rob.setPenColor(Color.RED);
+    	
+    	for (int i = 0; i < 5; i++) {
+			rob.move(20);
+			rob.turn(144);
+		}
     }
 
 
--- src/HappyNewYear.java
+++ src/HappyNewYear.java
@@ -1,34 +1,51 @@
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 // This recipe adds random numbers until the sum is exactly 2018!!!
 //     Happy New Year!
 
 public class HappyNewYear {
 	public static void main(String[] args) {
 		// 1. Make a variable 'newYear' and initialize it to 2018.
-
+		int newYear = 2018;
 
 		// 7. Repeat steps 2 to 6 (10 times)
 
-			// 2. Make a variable 'sum' and initialize to 0
-
+		// 2. Make a variable 'sum' and initialize to 0
+		for (int j = 0; j < 10; j++) {
 			
-			// 3. Repeat steps 4 to 6 (500 times) 
-
-				// 4. Get a random number between 0 and 50
-
-				
-				// 5. If sum is less than newYear then add your random
-				//    number to your sum, otherwise subtract it.
-				
-
-				// 6. If your sum matches newYear, then print the number of tries
-				//    to the console and use the "break" statement to exit the loop
-				
-
 		
+		int sum = 0;
+
+		// 3. Repeat steps 4 to 6 (500 times)
+
+		// 4. Get a random number between 0 and 50
+		Random number = new Random();
+		for (int i = 0; i < 500; i++) {
+
+			int x = number.nextInt(51);
+
+			// 5. If sum is less than newYear then add your random
+			// number to your sum, otherwise subtract it.
+			if (sum < newYear) {
+				sum = sum + x;
+			} else {
+				sum = sum - x;
+			}
+
+			// 6. If your sum matches newYear, then print the number of tries
+			// to the console and use the "break" statement to exit the loop
+			if (sum >= newYear) {
+				JOptionPane.showMessageDialog(null, "It took you " + i + " amount of tries");
+				break;
+			}
+			}
+
+		}
+
 		// *** BONUS: Keep track of your best score and worst score, and print
-		//            the results to the console.
-		
+		// the results to the console.
+
 	}
 }
